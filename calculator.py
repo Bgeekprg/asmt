9991015b114ee7e4fc3bdac7e6035d021ab77446
Bgeekprg <bhaveshpr54@gmail.com>
2025-01-19 22:04:01 +0530

    First commit

---
+++ calculator.py
@@ -0,0 +1,30 @@
+# Menu for showing operation list
+def menu():
+    print("1. + ")
+    print("2. - ")
+    print("3. * ")
+    print("4. / ")
+    print("5. % ")
+    print("6. ^ ")
+
+# Calculating Operations
+def calculate(choice, num1, num2):
+    pass
+
+  
+    
+   
+
+
+# Calling Menu
+menu()
+
+# Getting input of selection of menu item.
+choice = int(input("Enter Your Choice = "))
+
+# Getting inputs of two numbers.
+num1 = eval(input("Enter num 1 = "))
+num2 = eval(input("Enter num 2 = "))
+
+# Passing parameters in calculate method and print return value
+print(calculate(choice, num1, num2))
